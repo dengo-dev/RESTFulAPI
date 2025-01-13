2eac6bdbb2315f539d645c5521bb82339773f12d
dengo23-dev <dengo23.dev@gmail.com>
2025-01-13 23:49:00 +0900

    Member 생성 테스트

--- src/test/java/org/zerock/ex3/member/repository/MemberRepositoryTests.java
+++ src/test/java/org/zerock/ex3/member/repository/MemberRepositoryTests.java
@@ -1,13 +1,32 @@
 package org.zerock.ex3.member.repository;
 
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.zerock.ex3.member.entity.MemberEntity;
 
 @SpringBootTest
 public class MemberRepositoryTests {
   
   @Autowired
   private PasswordEncoder passwordEncoder;
+  
+  @Autowired
+  private MemberRepository memberRepository;
+  
+  @Test
+  public void testInsert() {
+    for (int i = 1; i <= 100; i++) {
+      MemberEntity memberEntity = MemberEntity.builder()
+          .mid("user" + i)
+          .mpw(passwordEncoder.encode("1111"))
+          .mname("USER" + i)
+          .email("user" + i + "@aaa.com")
+          .role(i <= 80 ? "USER:" : "ADMIN")
+          .build();
+      memberRepository.save(memberEntity);
+    }
+  }
 }
