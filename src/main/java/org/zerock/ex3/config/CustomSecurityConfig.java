190296a8aa88f9ee434614d1f37f3fdc70e3d6de
dengo <dengo23.dev@gmail.com>
2025-01-14 10:08:56 +0900

    Security Config설정

--- src/main/java/org/zerock/ex3/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/ex3/config/CustomSecurityConfig.java
@@ -5,12 +5,37 @@ import lombok.Builder;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Log4j2
 public class CustomSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    log.info("filter chain...................");
+
+    httpSecurity.formLogin(httpSecurityFormLoginConfigurer -> {
+      httpSecurityFormLoginConfigurer.disable();
+    });
+
+    httpSecurity.logout(config -> config.disable());
+    httpSecurity.csrf(config -> {
+      config.disable();
+    });
+
+    httpSecurity.sessionManagement(sessionManagementConfigurer -> {
+      sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.NEVER); //세션을 절대로 생생하지 않음
+    });
+
+
+
+    return httpSecurity.build();
+  }
   
   @Bean
   public PasswordEncoder passwordEncoder() {
