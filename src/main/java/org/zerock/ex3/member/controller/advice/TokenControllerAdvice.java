2fd859011bba69c03c3ecb8ced9f7be101273f13
dengo <dengo23.dev@gmail.com>
2025-01-14 11:43:18 +0900

    예외처리

--- src/main/java/org/zerock/ex3/member/controller/advice/TokenControllerAdvice.java
+++ src/main/java/org/zerock/ex3/member/controller/advice/TokenControllerAdvice.java
@@ -2,11 +2,14 @@ package org.zerock.ex3.member.controller.advice;
 
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.zerock.ex3.member.exception.MemberTaskException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -23,5 +26,16 @@ public class TokenControllerAdvice {
     Map<String, String> map = Map.of("error", msg);
 
     return ResponseEntity.status(status).body(map);
+  }
+
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException exception) {
+    log.info("handleAccessDeniedException...............");
+
+    Map<String, Object> errors = new HashMap<>();
+    errors.put("message", exception.getMessage());
+
+    return new ResponseEntity<>(errors, HttpStatus.FORBIDDEN);
+
   }
 }
