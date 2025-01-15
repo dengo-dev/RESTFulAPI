627ba5c8d0c4652b381bbc395f5cdaea2e7ab79c
dengo <dengo23.dev@gmail.com>
2025-01-15 11:03:05 +0900

    Review Entity

---
+++ src/main/java/org/zerock/ex3/review/entity/ReviewEntity.java
@@ -0,0 +1,43 @@
+package org.zerock.ex3.review.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.AttributeAccessor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.zerock.ex3.product.entity.ProductEntity;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_reviews", indexes = @Index(columnList = "product_pno")) //상품 번호를 빠르게 조회하기 위해
+@Getter
+@ToString(exclude = "productEntity")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(value = {AuditingEntityListener.class})
+public class ReviewEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long rno;
+
+  private String reviewText;
+
+  private String reviewer;
+
+  private int score;
+
+  @ManyToOne(fetch = FetchType.LAZY)//ProductEntity를 단 방향 참조
+  @JoinColumn(name = "product_pno") //ProductEntity는 ReviewEntity의 존재를 모른다.
+  private ProductEntity productEntity;
+
+  @CreatedDate
+  private LocalDateTime reviewDate;
+
+  @LastModifiedDate
+  private LocalDateTime modifiedDate;
+}
