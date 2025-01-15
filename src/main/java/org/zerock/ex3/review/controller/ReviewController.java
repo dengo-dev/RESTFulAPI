2eeb8c0de0a35e4af42a23be502ba94a309780a4
dengo <dengo23.dev@gmail.com>
2025-01-15 14:30:32 +0900

    Review 조회 && 삭제

--- src/main/java/org/zerock/ex3/review/controller/ReviewController.java
+++ src/main/java/org/zerock/ex3/review/controller/ReviewController.java
@@ -4,16 +4,15 @@ package org.zerock.ex3.review.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.ex3.review.dto.ReviewDTO;
 import org.zerock.ex3.review.exception.ReviewExceptions;
 import org.zerock.ex3.review.service.ReviewService;
 
 import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -32,5 +31,31 @@ public class ReviewController {
     }
     //ReviewTaskException은 RuntimeException계열이므로 별도의 예외처리가 필요하지 않다.
     return ResponseEntity.ok(reviewService.register(reviewDTO));
+  }
+
+  @GetMapping("/{rno}")
+  public ResponseEntity<ReviewDTO> read(@PathVariable("rno") Long rno) {
+    log.info("read: " + rno);
+
+    return ResponseEntity.ok(reviewService.read(rno));
+  }
+
+  @DeleteMapping("/{rno}")
+  public ResponseEntity<Map<String, String>> remove(@PathVariable("rno") Long rno, Authentication authentication) {
+    log.info("remove: " + rno);
+
+    String currentUser = authentication.getName();
+
+    log.info("currentUser: " + currentUser);
+
+    ReviewDTO reviewDTO = reviewService.read(rno);
+
+    if (!currentUser.equals(reviewDTO.getReviewer())) {
+      throw ReviewExceptions.REVIEWER_MISMATCH.get();
+    }
+
+    reviewService.remove(rno);
+
+    return ResponseEntity.ok().body(Map.of("result", "success"));
   }
 }
